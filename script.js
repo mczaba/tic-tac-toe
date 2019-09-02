f269854651b46be17a05a2beff70734d0c2bf6e1
Martin C <martin.czabadaj@gmail.com>
2019-09-02 17:35:08 +0200

    click event listener added and turns added

--- script.js
+++ script.js
@@ -1,25 +1,79 @@
+const displayBoard = document.querySelector("#displayBoard");
+const cases = document.querySelectorAll(".uneCase");
+const turnDisplayer = document.querySelector("#turnDisplayer");
+
+
 const gameBoard = (() => {
-    const board = [["empty","empty","empty"],["empty","empty","empty"],["empty","empty","empty"]];
-    const write = (a,b,string) => {board[a][b] = string;}
+    const board = ["","","","","","","","",""];
+    const write = (caseNumber,string) => {board[caseNumber] = string;}
     const show = () => {
-        console.log(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
-        console.log(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
-        console.log(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+        console.log(board[0] + "|" + board[1] + "|" + board[2]);
+        console.log(board[3] + "|" + board[4] + "|" + board[5]);
+        console.log(board[6] + "|" + board[7] + "|" + board[8]);
     }
-    return {write, show};
+    const render = () => {
+        
+        for (let i =0; i<9; i++){
+            if (board[i] === 1){
+                cases[i].textContent = "X";
+            }
+            else if (board[i] === 2){
+                cases[i].textContent = "O";
+            }
+            else {
+                cases[i].textContent = "";
+            }
+        }
+    }
+
+    return {write, show, render};
 })();
 
-const player = (name) => {
+const player = (name, number) => {
     const getName = () => name;
-    const play = (a,b) => {gameBoard.write(a,b,name);}
+    const play = (caseNumber) => {
+        gameBoard.write(caseNumber,number);
+        gameBoard.render();
+    }
     return {getName, play};
 };
 
-const martin = player("martin");
-const helene = player("helene");
+const martin = player("martin", 1);
+const helene = player("helene", 2);
 
-console.log(martin.getName());
-console.log(helene.getName());
-martin.play(1,1);
-helene.play(0,2);
-gameBoard.show();
+const gameController =((player1, player2) => {
+    let turn = player1;
+    const turnChange = () => {
+        if (turn === player1){turn = player2;}
+        else {turn = player1;}
+        turnDisplay();
+    }
+    const playerTurn = () => turn;
+    const turnDisplay = () => {
+        turnDisplayer.textContent = turn.getName() + "'s turn";
+    }
+    return {turnChange, playerTurn, turnDisplay}
+})(martin, helene);
+
+
+
+cases.forEach((div) => {
+    div.addEventListener("click", () => {
+        let casesArray = Array.from(cases);
+        let index = casesArray.indexOf(div);
+        gameController.playerTurn().play(index);
+        gameController.turnChange();
+    })
+})
+
+gameController.turnDisplay();
+// console.log(gameController.playerTurn());
+// gameController.turnChange();
+// console.log(gameController.playerTurn());
+// gameController.turnChange();
+// console.log(gameController.playerTurn());
+martin.play(4);
+// helene.play(2);
+// gameBoard.show();
+// gameBoard.render();
+// gameBoard.render();
